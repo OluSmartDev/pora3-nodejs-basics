3a1d68db446be3378b902b51a349903f77101338
OluSmartDev <oolalere2015@gmail.com>
2024-05-10 17:02:45 +0100

    add students fullName function

--- function.js
+++ function.js
@@ -4,3 +4,13 @@ function myFunction1() {
 }
 
 myFunction1();
+
+// Student's Full Name
+
+function students(firstName, lastName) {
+    const fullName = firstName + " " + lastName
+    console.log("Full Name of the student is: " + fullName)
+}
+
+// call the function with the arguments. Remember that yur arguments are strings, so they must be enclosed in " "
+students("Olusegun", "Olalere")
